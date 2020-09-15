42d1d23a6801d2eac237c9d71ba1d1bd3005a2e4
kioria95 <kenkioria@gmail.com>
2020-09-15 15:31:30 +0300

    Added Material UI and imported Button, Input, InputLabel, FormControl. Also added some state variables for messages and input and was able to map the messages and render them out

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React,{useState}from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import "./App.css";
 
 function App() {
-  const[input, setInput] =useState('');
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (event) => {
+    //Disables refresh when you put press the button to submit message as it is in a form
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
+  console.log(input);
+  console.log(messages);
   return (
     <div className="app">
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
 
-      {/*Input Field */}
-
-      {/*Button */}
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
+        </FormControl>
+      </form>
 
       {/*Messages */}
-     
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
