1666b952130b3fe402955b6f54de2e6d2ae4bea0
kioria95 <kenkioria@gmail.com>
2020-09-15 17:18:18 +0300

    Added itenary to message.js to determine which style to use for the message

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import Message from "./Message";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    {username: 'Kioria', text: 'I am mucheso'},
+    {username:'Tony Stark', text:'I am iron man'}
+  ]);
+  const [username, setUsername] = useState("");
+
+  //useState is how define variables in React
+  //useEffect is piece of code that runs when the dependency changes
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
 
   const sendMessage = (event) => {
     //Disables refresh when you put press the button to submit message as it is in a form
     event.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, {username: username, text: input}]);
     setInput("");
   };
 
-  console.log(input);
-  console.log(messages);
   return (
-    <div className="app">
+    <div className="App">
       <form>
         <FormControl>
           <InputLabel>Enter a message...</InputLabel>
@@ -39,7 +48,7 @@ function App() {
 
       {/*Messages */}
       {messages.map((message) => (
-        <p>{message}</p>
+        <Message message={message} username ={username} />
       ))}
     </div>
   );
